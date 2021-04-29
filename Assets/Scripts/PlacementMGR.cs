d5adcadd4617faadf2f84057e6a02d44d10a71fa
jmbenck <jmbenck@gmail.com>
2021-04-29 12:40:04 -0300

    Initial Commit
    Added Assets Folder

---
+++ Assets/Scripts/PlacementMGR.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class PlacementMGR : MonoBehaviour
+{
+
+    public GameObject placementIndicator;
+
+    [SerializeField]
+    private ARSessionOrigin arSessionOrigin;
+    [SerializeField]
+    private ARRaycastManager arRaycastManager;
+
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    [SerializeField]
+    TrackableType trackableType = TrackableType.Planes;
+
+    public GameObject objectToPlace;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = arSessionOrigin.camera.ViewportToScreenPoint(new Vector3(0.5f, 0, 5f));
+        var hits = new List<ARRaycastHit>();
+
+        arRaycastManager.Raycast(screenCenter, hits, trackableType);
+
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = arSessionOrigin.camera.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
